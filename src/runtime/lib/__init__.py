100993ce55edbb7bd6c4689005911bb44a748d58
yuzhigang <yuzhigang5460@163.com>
2026-04-14 16:48:22 +0800

    feat: export public lib API

---
+++ src/runtime/lib/__init__.py
@@ -0,0 +1,26 @@
+from src.runtime.lib.decorator import lib_function
+from src.runtime.lib.registry import LibRegistry
+from src.runtime.lib.proxy import LibProxy
+from src.runtime.lib.sandbox import SandboxExecutor, SAFE_BUILTINS
+from src.runtime.lib.exceptions import (
+    LibNotFoundError,
+    LibExecutionError,
+    LibRegistrationError,
+    ScriptExecutionError,
+    ImmutableContextError,
+    LibValidationError,
+)
+
+__all__ = [
+    "lib_function",
+    "LibRegistry",
+    "LibProxy",
+    "SandboxExecutor",
+    "SAFE_BUILTINS",
+    "LibNotFoundError",
+    "LibExecutionError",
+    "LibRegistrationError",
+    "ScriptExecutionError",
+    "ImmutableContextError",
+    "LibValidationError",
+]
